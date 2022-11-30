86bfb4f2af31907c334fb37d65d31cc1e762fe8a
onesimo martins <Ogouveia86@gmail.com>
2022-11-30 01:12:37 +0100

    Implementação do método SalvarCartao

--- src/main/java/com/sysmap/miniautorizador/domain/service/CartaoService.java
+++ src/main/java/com/sysmap/miniautorizador/domain/service/CartaoService.java
@@ -1,7 +1,30 @@
 package com.sysmap.miniautorizador.domain.service;
 
+import com.sysmap.miniautorizador.domain.exception.CartaoJaExistenteException;
+import com.sysmap.miniautorizador.domain.model.Cartao;
+import com.sysmap.miniautorizador.domain.repository.CartaoRepository;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class CartaoService {
+
+    private CartaoRepository cartaoRepository;
+
+    public CartaoService(CartaoRepository repository){
+        this.cartaoRepository=repository;
+    }
+
+    public Cartao salvarCartao(Cartao cartao){
+
+        cartaoRepository.findCartaoByNumeroCartao(cartao.getNumeroCartao())
+                .ifPresent( (i)-> {
+                    throw new CartaoJaExistenteException(
+                    String.format(  "cartão com o número '%s' já existente",cartao.getNumeroCartao())
+                    );
+                });
+
+          return this.cartaoRepository.save(cartao);
+    }
+
 }
