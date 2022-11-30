efef03a53f9146ec11564fa74135af737b229caf
onesimo martins <Ogouveia86@gmail.com>
2022-11-30 22:06:45 +0100

    criação de teste unitário para a conversão de Cartao para CartaoResponse

--- src/test/java/com/sysmap/miniautorizador/api/mapper/CartaoDtoMapperTest.java
+++ src/test/java/com/sysmap/miniautorizador/api/mapper/CartaoDtoMapperTest.java
@@ -1,8 +1,11 @@
 package com.sysmap.miniautorizador.api.mapper;
 
 import com.sysmap.miniautorizador.api.dto.input.NovoCartaoInput;
+import com.sysmap.miniautorizador.api.dto.response.CartaoResponse;
 import com.sysmap.miniautorizador.domain.model.Cartao;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,8 +23,23 @@ public class CartaoDtoMapperTest {
         assertAll(
                 ()->assertEquals(cartao.getNumeroCartao(),cartaoInput.getNumeroCartao()),
                 ()->assertEquals(cartao.getSenha(),cartaoInput.getSenha()),
-                ()->assertEquals(cartao.getSaldo(),null)
+                ()->assertNull(cartao.getSaldo())
+        );
+    }
+    @Test
+    public void deve_converter_cartao_para_cartaoResponse(){
+        Cartao cartao=new Cartao();
+        cartao.setSaldo(BigDecimal.valueOf(300));
+        cartao.setNumeroCartao("0987635467816253");
+        cartao.setSenha("1234");
+
+        CartaoResponse cartaoResponse= cartaoDtoMapper.toCartaoResponse(cartao);
+        assertAll(
+                ()->assertEquals(cartaoResponse.getNumeroCartao(),cartao.getNumeroCartao()),
+                ()->assertEquals(cartaoResponse.getSenha(),cartao.getSenha())
         );
 
     }
+
+
 }
