ca384e3c56704af9b06f01f1696252abf3a1fef5
onesimo martins <Ogouveia86@gmail.com>
2022-12-01 19:23:52 +0100

    criação do endpoint obter saldo

--- src/main/java/com/sysmap/miniautorizador/api/controller/CartaoRestController.java
+++ src/main/java/com/sysmap/miniautorizador/api/controller/CartaoRestController.java
@@ -7,13 +7,15 @@ import com.sysmap.miniautorizador.domain.model.Cartao;
 import com.sysmap.miniautorizador.domain.service.CartaoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
+import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("cartoes")
@@ -37,6 +39,11 @@ public class CartaoRestController {
        );
 
         return cartaoDtoMapper.toCartaoResponse(cartaoSalvo);
+    }
+
+    @GetMapping(value = "/{numeroCartao}")
+    public BigDecimal obterSaldo(@PathVariable String numeroCartao){
+        return  this.cartaoService.findCartaoByNumeroOrThrows(numeroCartao).getSaldo();
     }
 
 }
