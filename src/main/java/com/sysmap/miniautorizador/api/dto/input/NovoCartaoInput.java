45532e5f4d2213d5b535a2274eafc33e25b62a88
onesimo martins <Ogouveia86@gmail.com>
2022-12-01 09:43:33 +0100

    Adição do validator NotBlank no dto NovoCartaoInput

--- src/main/java/com/sysmap/miniautorizador/api/dto/input/NovoCartaoInput.java
+++ src/main/java/com/sysmap/miniautorizador/api/dto/input/NovoCartaoInput.java
@@ -1,5 +1,6 @@
 package com.sysmap.miniautorizador.api.dto.input;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 public class NovoCartaoInput {
@@ -9,9 +10,11 @@ public class NovoCartaoInput {
      * @senha regex que permite validar uma “string” (composta por números) de 4 dígitos.
      **/
     @Pattern(regexp = "^[0-9]{16}")
+    @NotBlank
     private final String numeroCartao;
 
     @Pattern(regexp = "^[0-9]{4}")
+    @NotBlank
     private final String senha;
 
     public NovoCartaoInput(String numeroCartao, String senha) {
