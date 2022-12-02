04cebf74fe8a917c8e4feafa64fc4df43d0f3fd5
onesimo martins <Ogouveia86@gmail.com>
2022-12-02 12:01:16 +0100

    criação da classe de domínio Transacao

---
+++ src/main/java/com/sysmap/miniautorizador/domain/model/Transacao.java
@@ -0,0 +1,26 @@
+package com.sysmap.miniautorizador.domain.model;
+
+import java.math.BigDecimal;
+
+public class Transacao {
+
+    private Cartao cartao;
+    private BigDecimal valor;
+
+
+    public Cartao getCartao() {
+        return cartao;
+    }
+
+    public void setCartao(Cartao cartao) {
+        this.cartao = cartao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+}
